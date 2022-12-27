7c84a12f8a84407e678fb89e93d8ec1a88c3b6cf
rom <filpoyma@gmail.com>
2022-12-27 13:35:36 +0300

    pupup processing

--- client/src/components/PopUp/PopUpCodeReviewMessage.jsx
+++ client/src/components/PopUp/PopUpCodeReviewMessage.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Button from '@mui/material/Button';
 import { SnackbarProvider, useSnackbar } from 'notistack';
 import { useSelector } from 'react-redux';
+import { getCurrentHour, getCurrentMinutes, getCurrentWeekDay } from '../../libs/functions';
 
 function PopUpComp({ message, variant }) {
   // variant could be success, error, warning, info, or default
@@ -12,9 +13,40 @@ function PopUpComp({ message, variant }) {
     let count = 0;
     let id = 0;
     if (popUpData.groupsCrTables.length && popUpData.teachersAndGaps.length) {
+      const currentTime = new Date();
+      const timeGaps = popUpData.teachersAndGaps.reduce((acc, data) => {
+        return {
+          ...acc,
+          [data.groupType]: data.timegaps.map((timegap) => {
+            const startCRTime = timegap.split('-')[0].split(':');
+            currentTime.setHours(Number(startCRTime[0]));
+            currentTime.setMinutes(Number(startCRTime[1]));
+            return currentTime.getTime();
+          })
+        };
+      }, {});
+
+      const currentWeekDay = getCurrentWeekDay();
+
+      const filteredByCurrentDay = popUpData.groupsCrTables.map((table) =>
+        table.crtables.filter((crtables) => crtables.crDay === currentWeekDay)
+      );
+      // console.log('file-PopUpCodeReviewMessage.jsx filteredByCurrentDay:', filteredByCurrentDay);
+
+      // const filteredByCurentHours = filteredByCurrentDay.map((crtables) =>
+      //   crtables.map((crtable) =>
+      //     crtable.tableData.map((row,i) => ())
+      //
+      //
+      //   )
+      // );
+      // .map((data) => data.tableData.map((tableData, i) => tableData))
+
+      var time = '10:00-10:10'.match(/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/i);
+
       id = setInterval(() => {
         popUpHandler(++count, variant);
-      }, 3000);
+      }, 30000);
     }
     return () => clearInterval(id);
   }, [popUpData]);
--- client/src/libs/functions.js
+++ client/src/libs/functions.js
@@ -1,1 +1,17 @@
 export const isObjEmpty = (obj = {}) => Object.keys(obj).length === 0;
+
+export const isTimeGapsValid = (timegaps) => {
+  const matchTime = (times) => {
+    const matchedGaps = times.match(/([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/gi);
+    return matchedGaps === null ? [] : matchedGaps;
+  };
+  return timegaps.split(/ *, */g).every((times) => matchTime(times).length === 2);
+};
+
+export const getCurrentWeekDay = () => {
+  const days = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+  return days[new Date().getDay()];
+};
+
+export const getCurrentHour = () => new Date().getHours();
+export const getCurrentMinutes = () => new Date().getMinutes();
