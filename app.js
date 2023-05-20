09331c05c092f4cf3781a57864b285b84ccbe039
grentank <mexmat100394@gmail.com>
2023-05-20 19:40:08 +0300

    temporary disabled mongoose to gain IP

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const express = require('express');
 
 const indexRouter = require('./routes/index');
-const groupsRouter = require('./routes/groups');
-const schemasRouter = require('./routes/schemas');
-const teachersRouter = require('./routes/teachers');
-const studentsRouter = require('./routes/students');
-const examRouter = require('./routes/exam');
+// const groupsRouter = require('./routes/groups');
+// const schemasRouter = require('./routes/schemas');
+// const teachersRouter = require('./routes/teachers');
+// const studentsRouter = require('./routes/students');
+// const examRouter = require('./routes/exam');
 const { addMiddlewares, addErrorHandlers } = require('./middlewares/add-middlewares');
 const { PORT } = require('./config/constants');
 
@@ -17,12 +17,11 @@ app.use(express.static(path.resolve('client', 'build')));
 addMiddlewares(app);
 
 app.use('/api', indexRouter);
-app.use('/api/groups', groupsRouter);
-app.use('/api/schemas', schemasRouter);
-app.use('/api/teachersandtime', teachersRouter);
-app.use('/api/students', studentsRouter);
-app.use('/api/exam', examRouter);
-
+// app.use('/api/groups', groupsRouter);
+// app.use('/api/schemas', schemasRouter);
+// app.use('/api/teachersandtime', teachersRouter);
+// app.use('/api/students', studentsRouter);
+// app.use('/api/exam', examRouter);
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve('client', 'build', 'index.html'));
--- seeders/createDefaultGroups.js
+++ seeders/createDefaultGroups.js
@@ -1,27 +1,27 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 require('dotenv').config();
-require('../connection');
-const Group = require('../models/Group');
+// require('../connection');
+// const Group = require('../models/Group');
 
-const groups = [
-  new Group({
-    name: 'WaitList',
-    groupType: 'waitlist'
-  }),
-  new Group({
-    name: 'Inactive',
-    groupType: 'inactive'
-  })
-];
-mongoose.connection.on('connected', function () {
-  Group.create(groups)
-    .then((res) => {
-      console.log('Created:', res);
-      mongoose.disconnect();
-    })
-    .catch((err) => {
-      console.error('Seeds failed', err.message);
-      mongoose.disconnect();
-    });
-});
+// const groups = [
+//   new Group({
+//     name: 'WaitList',
+//     groupType: 'waitlist'
+//   }),
+//   new Group({
+//     name: 'Inactive',
+//     groupType: 'inactive'
+//   })
+// ];
+// mongoose.connection.on('connected', function () {
+//   Group.create(groups)
+//     .then((res) => {
+//       console.log('Created:', res);
+//       mongoose.disconnect();
+//     })
+//     .catch((err) => {
+//       console.error('Seeds failed', err.message);
+//       mongoose.disconnect();
+//     });
+// });
 
