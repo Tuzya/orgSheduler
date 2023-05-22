c4692bbf4b5ff24f7081d593bd423857fbeb8c11
grentank <mexmat100394@gmail.com>
2023-05-22 16:06:41 +0300

    fixed error

--- client/src/pages/GroupPage/cutNames.js
+++ client/src/pages/GroupPage/cutNames.js
@@ -1,5 +1,5 @@
 export default function cutNames(firstAndSecondName) {
-  if (!firstAndSecondName.includes(' ')) return firstAndSecondName;
+  if (!firstAndSecondName || !firstAndSecondName?.includes(' ')) return firstAndSecondName;
   const [firstName, secondName] = firstAndSecondName.split(' ');
   const reducedSecondName = `${secondName[0].toUpperCase()}-${secondName.split('').at(-1)}`;
   return `${firstName} ${reducedSecondName}`;
